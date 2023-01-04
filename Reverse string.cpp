f94ae6a0a77a342b216ecef8573b4648292da5a5
IshitaJain <91709949+ishita126jain@users.noreply.github.com>
2023-01-04 13:57:03 +0530

    Create Reverse string.cpp

---
+++ Reverse string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        int st=0;
+        int en=n-1;
+        while(st<en){
+            swap(s[st],s[en]);
+            st++;
+            en--;
+        }
+    }
+};
