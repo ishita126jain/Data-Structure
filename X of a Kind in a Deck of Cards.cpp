ca82952b1c277b76951ccab9ce3a03fa0e8364f5
IshitaJain <91709949+ishita126jain@users.noreply.github.com>
2023-04-06 19:48:08 +0530

    Create X of a Kind in a Deck of Cards.cpp

---
+++ X of a Kind in a Deck of Cards.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        map <int,int> mp;
+        vector <int> v;
+        if(deck.size()==1)
+            return false;
+        for (int i:deck)
+            mp[i+1]++;
+        for(auto it:mp){
+            v.push_back(it.second);
+        }
+       
+        if(v.size()==1 && deck.size()!=1)
+            return true;
+        int a=__gcd(v[0],v[1]);
+        if(a==1)
+            return false;
+    
+        for(int i=2;i<v.size();i++){
+            if((__gcd(v[i],v[i-1]))==1)
+                return false;
+            else 
+                a=(__gcd(v[i],v[i-1]));
+
+        }
+        return true;
+    }
+};
