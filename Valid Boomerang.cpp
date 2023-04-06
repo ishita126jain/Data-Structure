f6762d61eea89de6658bea599651d24d7b5a079e
IshitaJain <91709949+ishita126jain@users.noreply.github.com>
2023-04-06 20:16:22 +0530

    Create Valid Boomerang.cpp

---
+++ Valid Boomerang.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        int a = points[0][0] * (points[1][1] - points[2][1]) +
+                points[1][0] * (points[2][1] - points[0][1]) +
+                points[2][0] * (points[0][1] - points[1][1]);
+        if(a==0){
+            return false;
+        }
+        return true;
+    }
+};
