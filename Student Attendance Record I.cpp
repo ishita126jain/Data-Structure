fae0bb2eab64d6d4c6b747bab96985df575d734e
IshitaJain <91709949+ishita126jain@users.noreply.github.com>
2023-04-20 18:54:37 +0530

    Create Student Attendance Record I.cpp

---
+++ Student Attendance Record I.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int i, size = s.size(), maxL=0, countA=0, countL=0;
+    for (i = 0; i < size; ++i) {
+        if (s[i] == 'L') {
+            ++countL;
+        } else {
+            countL = 0;
+        }
+        if (s[i] == 'A') {
+            ++countA;
+        }
+        if (maxL < countL) {
+            maxL = countL;
+        }
+        if( countA >= 2 || maxL >= 3) {
+            return false;
+        }
+    }
+    return true;
+
+    }
+};
